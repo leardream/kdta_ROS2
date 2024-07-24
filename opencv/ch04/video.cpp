de41e634c1833239742c98078b4d84ff3b6d01f2
jh0933 <jh0933.naver.com>
2024-07-24 16:52:44 +0900

    aa

--- opencv/ch04/video.cpp
+++ opencv/ch04/video.cpp
@@ -3,25 +3,31 @@
 
 using namespace cv;
 using namespace std;
-String folder = "/home/jh0933/다운로드/kdta_ROS2/opencv/data/";
+String folder = "/home/jh0933/다운로드/kdta_ROS2/opencv/data";
 
 int main()
 {
     Mat frame, doubleFrame, reshapeFrame;
-    VideoCapture cap(0, CAP_V4L2);
+    VideoCapture cap(folder + "vtest.avi");
 
     double fps = cap.get(CAP_PROP_FPS);
     int delay = cvRound(1000 / fps);
     Size sz1(cap.get(CAP_PROP_FRAME_WIDTH), cap.get(CAP_PROP_FRAME_HEIGHT));
     std::vector <int> shape = {sz1.height * 2, sz1.width / 2};
-    
+     
     if (!cap.isOpened()) {
         cerr << "Video open failed " << endl;
         return -1;
     }
     cout << "Video open" << endl;
+    int fourcc = VideoWriter::fourcc('D', 'I', 'V', 'X');
+    VideoWriter outputVideo(folder + "output10.avi", fourcc, fps, sz1); 
+
+
     while(true) {
         cap >> frame;
+        if (frame.empty())
+            break;
 
         resize(frame, doubleFrame, sz1 * 2);
         reshapeFrame = frame.reshape(3,shape);
@@ -29,11 +35,14 @@ int main()
         imshow("frame", frame);
         imshow("doubleFrame", doubleFrame);
         imshow("rashape", reshapeFrame);
+        
+        outputVideo << frame;
 
         if (waitKey(delay) == 27)
             break;
     }
     cap.release();
+    outputVideo.release();
 
     return 0;
 }
