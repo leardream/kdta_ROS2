f26bef8282e615820a12675568bb922fb7743b93
jh0933 <jh0933.naver.com>
2024-07-25 09:58:34 +0900

    aa

--- opencv/ch04/keyboard.cpp
+++ opencv/ch04/keyboard.cpp
@@ -1,5 +1,7 @@
 #include "opencv2/opencv.hpp"
 #include <iostream>
+#include <chrono>
+#include <thread>
 
 using namespace cv;
 using namespace std;
@@ -18,31 +20,43 @@ int main()
 
     namedWindow("img");
     imshow("img", img);
-    int fps = 30;
-    int delay = cvRound(1000 / 30);
+    float fps = 30.0;
+    // int delay = cvRound(1000 / fps);
     Scalar a(0, 0, 0);
+    TickMeter tm;
+    TickMeter tm2;
 
-    while (true) {
-        keycode = waitKey(delay);
+    while (true)
+    {
+        tm.start();
+        tm2.start();
+        keycode = waitKey(10);
         if (keycode != -1)
             cout << "keycode: " << keycode << endl;
         if (keycode == 27)
             break;
         if (keycode == 'i' || keycode == 'I') {
             img = ~img;
-            imshow("img", img);
+                imshow("img", img);
         }
         if (keycode == 'a' || keycode == 'A') {
             a = a + Scalar(1, 1, 1);
             img2 = img + a;
-            imshow("img", img2);
+                imshow("img", img2);
         }
         if (keycode == 'b' || keycode == 'B') {
             a = a - Scalar(1, 1, 1);
             img2 = img + a;
-            imshow("img", img2);
+                imshow("img", img2);
         }
+        tm.stop();
+        cout << "tm.getFPS(): " << tm.getFPS() << endl;
+        if (tm.getFPS() > fps) {
+            auto sleep_duration = static_cast<int>(1000.0 * (tm.getFPS() - fps) / tm.getFPS() / fps);
+            this_thread::sleep_for(chrono::milliseconds(sleep_duration));
+        }
+        tm2.stop();
+        cout << "tm2.getFPS(): " << tm2.getFPS() << endl;
     }
-
     return 0;
 }
