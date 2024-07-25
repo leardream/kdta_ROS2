5e1b746f117bf6044a69ba313833e757a26a02fe
jh0933 <jh0933.naver.com>
2024-07-26 08:58:02 +0900

    aa

--- README.md
+++ README.md
@@ -17,11 +17,18 @@
 - CmakeLists 기본구조와 활용법
 - c_cpp_properties.json 작성과 .gitignore를 활용한 기본셋팅
 - imread, imshow 등으로 이미지 출력
+    - String folder 를 이용해서 경로지정
 - Size, Scalar 를 활용한 화면구성
 
 ### 2024 - 07 - 24
 - matrix 연산자 사용예제들
-
+    - CV_8UC1
+        - CV : OpenCV를 의미
+        - 8 : bit, 8bit를 의미
+        - C1 : 채널을 의미, 채널이 1개
+    - BGR 순으로 색상입력
+    
+    
 
 
     
--- opencv/ch04/Test.cpp
+++ opencv/ch04/Test.cpp
@@ -4,56 +4,70 @@
 using namespace std;
 using namespace cv;
 
-string folder = "/home/jh0933/다운로드/kdta_ROS2/opencv/data/";
-
 void onMouse(int event, int x, int y, int flags, void *data);
-
-struct Mydata {
+struct Mydata
+{
     Point ptOld;
     Point ptNew;
     Mat img;
+    Mat background;
+    Scalar white = Scalar(255, 255, 255);
     Scalar yellow = Scalar(0, 255, 255);
     Scalar blue = Scalar(255, 0, 0);
     Scalar green = Scalar(0, 255, 0);
     Scalar red = Scalar(0, 0, 255);
-    Scalar white = Scalar(255, 255, 255);
+    vector<Vec3b> mouseBGR;
     bool flag = false;
 };
+
+string folder = "/home/jh0933/다운로드/kdta_ROS2/opencv/data/";
+
+void onMouse(int event, int x, int y, int flags, void *data);
 
 int main()
 {
     Mydata mydata;
-    Mat img = imread(folder + "lena.bmp");
+    FileStorage fs;
+    mydata.img = imread(folder + "lenna.bmp");
+    mydata.background = mydata.img.clone();
 
-    if (img.empty()) {
-        cout << "Could not load image." << endl;
-        return -1;
+    namedWindow("img");
+    setMouseCallback("img", onMouse, (void *)&mydata);
+
+    imshow("img", mydata.img);
+    int keycode = waitKey(0);
+    if (keycode == 27)
+    {
+        fs.open(folder + "mouseBGRPoint.json", FileStorage::WRITE);
+        fs << "mouseBGR" << mydata.mouseBGR;
+        fs.release();
     }
-
-    mydata.img = img.clone();
-    namedWindow("lena");
-    imshow("lena", img);
-
-    setMouseCallback("lena", onMouse, (void *)&mydata);
-
-    waitKey(0);
-    destroyWindow("lena");
-
+    destroyAllWindows();
     return 0;
 }
 
 void onMouse(int event, int x, int y, int flags, void *data)
 {
     Mydata *ptr = (Mydata *)data;
-    if (event == EVENT_MOUSEMOVE) {
-        ptr->img = imread(folder + "lena.bmp").clone();
-        rectangle(ptr->img, Point(x - 25, y - 25), Point(x + 25, y + 25), ptr->white, 2);
-        imshow("lena", ptr->img);
-    } else if (event == EVENT_LBUTTONDOWN) {
-        Vec3b bgr = ptr->img.at<Vec3b>(y, x);
-        cout << "BGR Value at (" << x << ", " << y << "): "
-             << "B=" << static_cast<int>(bgr[0]) << ", "
-             << "G=" << static_cast<int>(bgr[1]) << ", "
-             << "R=" << static_cast<int>(bgr[2]) << endl;
+    switch (event)
+    {
+    case EVENT_LBUTTONDOWN:
+        cout << "mouse left button down" << x << y << endl;
+        ptr->ptOld = Point(x, y);
+        ptr->mouseBGR.push_back(ptr->background.at<Vec3b>(y, x));
+        ptr->flag = true;
+        break;
+    case EVENT_LBUTTONUP:
+        cout << "mouse left button up" << x << y << endl;
+        ptr->flag = false;
+        break;
+    case EVENT_MOUSEMOVE:
+        if (EVENT_FLAG_LBUTTON)
+        {
+            ptr->background.copyTo(ptr->img);
+            rectangle(ptr->img, Rect(Point(x - 25, y - 25), Point(x + 25, y + 25)), ptr->red, 5);
+            imshow("img", ptr->img);
+            ptr->ptOld = Point(x, y);
+        }
     }
 }
